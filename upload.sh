1759456e061587f48957fbb9680b860ba662d2fe
huxins <huxins@163.com>
2020-01-09 14:36:13 +0800

    update

--- upload.sh
+++ upload.sh
@@ -3,14 +3,14 @@
 set -eux
 
 apk add curl
-curl -X GET -G --data-urlencode "text=pwd" --data-urlencode "desp=$(pwd)" \
-	"https://sc.ftqq.com/SCU39002T44ee92843b639edac4813ce6d566c3c45c3b0f775e97d.send"
-curl -X GET -G --data-urlencode "text=ls" --data-urlencode "desp=$(ls)" \
-	"https://sc.ftqq.com/SCU39002T44ee92843b639edac4813ce6d566c3c45c3b0f775e97d.send"
-curl -X GET -G --data-urlencode "text=ls /" --data-urlencode "desp=$(ls /)" \
-	"https://sc.ftqq.com/SCU39002T44ee92843b639edac4813ce6d566c3c45c3b0f775e97d.send"
+# curl -X GET -G --data-urlencode "text=pwd" --data-urlencode "desp=$(pwd)" \
+# 	"https://sc.ftqq.com/SCU39002T44ee92843b639edac4813ce6d566c3c45c3b0f775e97d.send"
+# curl -X GET -G --data-urlencode "text=ls" --data-urlencode "desp=$(ls)" \
+# 	"https://sc.ftqq.com/SCU39002T44ee92843b639edac4813ce6d566c3c45c3b0f775e97d.send"
+# curl -X GET -G --data-urlencode "text=ls /" --data-urlencode "desp=$(ls /)" \
+# 	"https://sc.ftqq.com/SCU39002T44ee92843b639edac4813ce6d566c3c45c3b0f775e97d.send"
 
-pip install coscmd
+pip install --quiet --no-cache-dir coscmd
 
 coscmd config -a $INPUT_SECRET_ID -s $INPUT_SECRET_KEY -b $INPUT_BUCKETNAME_APPID -r $INPUT_REGION
 
