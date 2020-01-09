35a687a3e42402ee855162d2fa4ab5ed8922a57c
huxins <huxins@163.com>
2020-01-09 20:44:31 +0800

    update

--- upload.sh
+++ upload.sh
@@ -2,10 +2,6 @@
 
 set -eux
 
-if [ "${INPUT_FOLDER:-.}" = "yes" ];then
-	folder="-r"
-fi
-
 # apk add curl
 # curl -X GET -G --data-urlencode "text=pwd" --data-urlencode "desp=$(pwd)" \
 # 	"https://sc.ftqq.com/SCU39002T44ee92843b639edac4813ce6d566c3c45c3b0f775e97d.send"
@@ -18,4 +14,4 @@ pip install --quiet --no-cache-dir coscmd
 
 coscmd config -a $INPUT_SECRET_ID -s $INPUT_SECRET_KEY -b $INPUT_BUCKETNAME_APPID -r $INPUT_REGION
 
-coscmd upload $folder $INPUT_LOCALPATH $INPUT_COSPATH
+coscmd upload ${INPUT_FOLDER:+-r} $INPUT_LOCALPATH $INPUT_COSPATH
